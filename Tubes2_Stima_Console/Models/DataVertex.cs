d241e80b85f4dd6040a40f763d03ae50f0e291d7
miner46er <stefanus.ardimulia@gmail.com>
2019-03-14 23:51:06 +0700

    Created graph data types using GraphX and QuickGraph

---
+++ Tubes2_Stima_Console/Models/DataVertex.cs
@@ -0,0 +1,38 @@
+﻿using GraphX.PCL.Common.Models;
+
+namespace Tubes2_Stima_Console.Models
+{
+    /* DataVertex is the data class for the vertices. It contains all custom vertex data specified by the user.
+     * This class also must be derived from VertexBase that provides properties and methods mandatory for
+     * correct GraphX operations.
+     * Some of the useful VertexBase members are:
+     *  - ID property that stores unique positive identfication number. Property must be filled by user.
+     *  
+     */
+
+    public class DataVertex: VertexBase
+    {
+        /// <summary>
+        /// Some string property for example purposes
+        /// </summary>
+        public int NomorRumah { get; set; }
+
+        /// <summary>
+        /// Default parameterless constructor for this class
+        /// (required for YAXLib serialization)
+        /// </summary>
+        public DataVertex():this(0)
+        {
+        }
+
+        public DataVertex(int _NomorRumah = 0)
+        {
+            this.NomorRumah = _NomorRumah;
+        }
+        
+        public int ToInt()
+        {
+            return this.NomorRumah;
+        }
+    }
+}
---
+++ Tubes2_Stima_Console/Models/GXLogicCore.cs
@@ -0,0 +1,10 @@
+using GraphX.PCL.Logic.Models;
+using QuickGraph;
+
+namespace Tubes2_Stima_Console.Models
+{
+    /// <summary>
+    /// Logics core object which contains all algorithms and logic settings
+    /// </summary>
+    public class GXLogicCore : GXLogicCore<DataVertex, DataEdge, GraphKingdomMap> { }
+}
