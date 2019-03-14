7451c3a18e4fe3a88400122f1d33096309f3f036
miner46er <stefanus.ardimulia@gmail.com>
2019-03-14 14:12:08 +0700

    Add project files.

---
+++ Tubes2_Stima_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes2_Stima_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
