b650a1f3018d2c2cae25569a0e0a0750544d364d
miner46er <stefanus.ardimulia@gmail.com>
2019-03-18 02:50:59 +0700

    Add graph displaying functionality

--- Tubes2_Stima_WPF/MainWindow.xaml.cs
+++ Tubes2_Stima_WPF/MainWindow.xaml.cs
@@ -35,6 +35,18 @@ namespace Tubes2_Stima_WPF
             Peta.Parse();
 
             Find_Solution(Peta);
+
+            //Customize Zoombox a bit
+            //Set minimap (overview) window to be visible by default
+            ZoomControl.SetViewFinderVisibility(zoomctrl, Visibility.Visible);
+            //Set Fill zooming strategy so whole graph will be always visible
+            zoomctrl.ZoomToFill();
+
+            //Lets setup GraphArea settings
+
+            GraphKingdomArea_Setup(Peta.Map);
+
+            ShowGraph();
         }
 
         static void Find_Solution(MapReader Peta)
@@ -98,6 +110,51 @@ namespace Tubes2_Stima_WPF
                 }
                 Console.WriteLine();
             }
+        }
+
+        private void ShowGraph()
+        {
+            Area.GenerateGraph(true, true);
+
+            Area.SetEdgesDashStyle(EdgeDashStyle.Solid);
+
+            Area.ShowAllEdgesArrows(false);
+
+            zoomctrl.ZoomToFill();
+        }
+
+        private void GraphKingdomArea_Setup(GraphKingdomMap graph)
+        {
+            //create logic core and fill Graph
+            var logicCore = new GraphXLogicCore() { Graph = graph };
+            //This property sets layout algorithm that will be used to calculate vertices positions
+            //Different algorithms uses different values and some of them uses edge Weight property.
+            logicCore.DefaultLayoutAlgorithm = LayoutAlgorithmTypeEnum.KK;
+            //Now we can set parameters for selected algorithm using AlgorithmFactory property. This property provides methods for
+            //creating all available algorithms and algo parameters.
+            logicCore.DefaultLayoutAlgorithmParams = logicCore.AlgorithmFactory.CreateLayoutParameters(LayoutAlgorithmTypeEnum.KK);
+            //Unfortunately to change algo parameters you need to specify params type which is different for every algorithm.
+            ((KKLayoutParameters)logicCore.DefaultLayoutAlgorithmParams).MaxIterations = 100;
+
+            //This property sets vertex overlap removal algorithm.
+            //Such algorithms help to arrange vertices in the layout so no one overlaps each other.
+            logicCore.DefaultOverlapRemovalAlgorithm = OverlapRemovalAlgorithmTypeEnum.FSA;
+            //Default parameters are created automaticaly when new default algorithm is set and previous params were NULL
+            logicCore.DefaultOverlapRemovalAlgorithmParams.HorizontalGap = 50;
+            logicCore.DefaultOverlapRemovalAlgorithmParams.VerticalGap = 50;
+
+            //This property sets edge routing algorithm that is used to build route paths according to algorithm logic.
+            //For ex., SimpleER algorithm will try to set edge paths around vertices so no edge will intersect any vertex.
+            //Bundling algorithm will try to tie different edges that follows same direction to a single channel making complex graphs more appealing.
+            logicCore.DefaultEdgeRoutingAlgorithm = EdgeRoutingAlgorithmTypeEnum.SimpleER;
+
+            //This property sets async algorithms computation so methods like: Area.RelayoutGraph() and Area.GenerateGraph()
+            //will run async with the UI thread. Completion of the specified methods can be catched by corresponding events:
+            //Area.RelayoutFinished and Area.GenerateGraphFinished.
+            logicCore.AsyncAlgorithmCompute = false;
+
+            //Finally assign logic core to GraphArea object
+            Area.LogicCore = logicCore;
         }
     }
 }
